389f6e76686455f35fa2106e6cfa098d1ec08960
Md Iqbal Hossain <iqbal86571361@gmail.com>
2022-03-22 20:30:02 +0600

    Update footer.blade.php

--- resources/views/ecommerce/footer.blade.php
+++ resources/views/ecommerce/footer.blade.php
@@ -103,10 +103,10 @@
 
         <div class="footer-right ml-auto mt-1 mt-sm-0">
             <div class="payment-icons mr-0">
-                <span class="payment-icon visa" style="background-image: url(porto/asset(assets/images/payments/payment-visa.svg)"></span>
+                <!-- <span class="payment-icon visa" style="background-image: url(porto/asset(assets/images/payments/payment-visa.svg)"></span>
                 <span class="payment-icon paypal" style="background-image: url(porto/assets/images/payments/payment-paypal.svg)"></span>
                 <span class="payment-icon stripe" style="background-image: url(porto/assets/images/payments/payment-stripe.png)"></span>
-                <span class="payment-icon verisign" style="background-image:  url(porto/assets/images/payments/payment-verisign.svg)"></span>
+                <span class="payment-icon verisign" style="background-image:  url(porto/assets/images/payments/payment-verisign.svg)"></span> -->
             </div>
         </div>
     </div>
